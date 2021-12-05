3c5cf11700adb375207b5dbcbb2fc384300d3d76
illtellyoulater <3078931+illtellyoulater@users.noreply.github.com>
2021-12-05 01:32:03 +0100

    Update instagram.js
    This script was no longer working so I decided to fix it. 
    The approach it was using (trying to select and operate only on the current <video> element being looked at) was not working, as it had the problem that it kept referencing the already gone-off-screen video element, while having no effects at all on the current "focused" video element.
    My approach was to select and operate on all video elements at once. Not so elegant, but it works.

--- instagram.js
+++ instagram.js
@@ -1,14 +1,27 @@
+// I updated this script as the previous approach of trying to select and operate only on the current <video> element being looked at
+// was in reality not working. In fact it had the problem that it kept referencing an already gone-off-screen video element, 
+// having no effect on the current "focused" video element.
+// My approch to fix it was to select and operate on all video elements at once. Not so elegant, but it works.
+
 setTimeout(function keepRun() {
   setTimeout(keepRun, 1000);
+   
+  var arr = document.querySelectorAll('video');
   
-  var a = document.querySelector('video');
-
-  if(a) {
-    a.setAttribute("controls","controls");
-    var el = a.parentElement.parentElement.parentElement.parentElement
-    if (el.children.length > 1) {
-      el.removeChild(el.children[1]);
-      el.removeChild(el.children[1]);
+  arr.forEach(function(videoel) {
+    videoel.setAttribute("controls","controls");
+    var containerel = videoel.parentElement.parentElement.parentElement.parentElement;
+    if (containerel.children.length > 1) {
+      containerel.removeChild(containerel.lastChild); 
     }
-  }
+    
+    // We also need to not displaying the img element right after <video>.
+    // It can't just be deleted or there will be a cross-origin error
+    videoel.nextSibling.style.display = 'none';
+    
+    // just a quick volume "fix" (better this way than always starting videos at full volume)
+    videoel.volume = 0.07; 
+    
+  });
+  
 }, 1000);
